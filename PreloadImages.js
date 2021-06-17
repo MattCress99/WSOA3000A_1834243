c40e30cc90a86580ac83e80e3ae1843e80f7c3ca
MattCress99 <62275852+MattCress99@users.noreply.github.com>
2021-06-17 20:51:50 +0200

    added a preload images function and some metadata

---
+++ PreloadImages.js
@@ -0,0 +1,44 @@
+var images = new Array()
+function preLoadImages()
+{
+    for(i=0; i < preLoadImages.arguments.length; i++)
+    {
+        images[i] = new Image()
+        images[i].src = preLoadImages.arguments[i]
+    }
+}
+
+preLoadImages(
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/Space-5200.mp4",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/Breakfastclub_vol1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/Breakfastclub_vol2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CameronRexIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CameronRexIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CameronRexIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CameronSchreurIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CameronSchreurIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CameronSchreurIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CamronIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CamronIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/CamronIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/close.png.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/ethanIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/ethanIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/ethanIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/facebook.png",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/instagram.png",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/JPIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/JPIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/JPIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/menu.png",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/NTFSIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/NTFSIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/NTFSIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/ShaylinIMG1.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/ShaylinIMG2.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/ShaylinIMG3.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/twitter.png",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/Wireframes0001.jpg",
+    "https://mattcress99.github.io/WSOA3028A_1834243/images/wireframes2.jpg"
+
+)
