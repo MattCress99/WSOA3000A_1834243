6979b31cfef6d4c0e03013adf28d1535a4c8808b
MattCress99 <62275852+MattCress99@users.noreply.github.com>
2021-06-17 10:40:27 +0200

    file restructuring, styling update
    -restructured the filing system of the project to make it more ordered
    -updated blog section to make it more readable
    -tried to fix styling so that website works on phones too
    -begun implementation of a read more function for blog posts

---
+++ ShowMoreTextScript.js
@@ -0,0 +1,19 @@
+function toggleText()
+{
+    var points = document.getElementById("points");
+    var showMoreText = document.getElementById("moreText");
+    var buttonText = document.getElementById("textButton");
+    showMoreText.style.display="none";
+    if(points.style.display == "none")
+    {
+        showMoreText.style.display = "none";
+        points.style.display = "inline";
+        buttonText.innerHTML = "Show More";
+    }
+    else
+    {
+        showMoreText.style.display = "inline";
+        points.style.display = "none";
+        buttonText.innerHTML = "Show Less";
+    }
+}
