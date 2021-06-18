65cad73e34e2090021676426f08742a3a1bf58fe
MattCress99 <62275852+MattCress99@users.noreply.github.com>
2021-06-18 12:57:48 +0200

    made entry fields clear on contact page

---
+++ ContactPage/ClearInputFields.js
@@ -0,0 +1,12 @@
+var Firstname = document.getElementById('fname')
+var Surname = document.getElementById('sName')
+var Country = document.getElementById('country')
+var Subject = document.getElementById('subject')
+
+function Clear()
+{
+    Firstname.value=''
+    Surname.value=''
+    Country.value=''
+    Subject.value=''
+}
